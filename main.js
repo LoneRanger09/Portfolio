bd14ea0983f149419160d9fbe4f7dd1a544f37ab
LoneRanger09 <152678545+LoneRanger09@users.noreply.github.com>
2025-04-19 23:36:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+{
+<script>
+document.addEventListener("DOMContentLoaded", () = {
+  new Typer("#typer", {
+    strings: ["Web Developer", "Programmer", "Tech Enthusiast", "Open Source Contributor"],
+    typeSpeed: 50,
+    backspaceSpeed: 30,
+    backspaceDelay: 1000,
+    loop: true,
+  })
+});
+</script>
+}
+
+
